90ab92e59e9da79eabb1a0cc95c75633de743f3d
trevin-livele <liveletrevin6@gmail.com>
2022-02-15 11:52:49 +0300

    initila files

---
+++ user.py
@@ -0,0 +1,16 @@
+#user.py
+
+class User():
+
+
+    def __init__(self,id,username,password):
+
+
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+
+        def __str__(self):
+            return f"User Id: {self.id}"
